dc7516dcfbacacab5417a71d2bcc72f9d0bdfdec
vjraghavtest <41062314+vjraghavtest@users.noreply.github.com>
2018-07-13 15:23:05 +0530

    Multi req handling

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -29,156 +29,157 @@ public class ClientHandler extends Thread {
 		PrintWriter printWriter = null;
 		BufferedInputStream inputStream = null;
 		BufferedOutputStream outputStream = null;
-		String owner = null, filename = null;
+		String owner = null, filename = null, data = null;
 		HashMap<String, String> file = null;
 		int bytesRead = 0;
 		long filesize = 0;
-		int bufferSize = 1024 * 1024 ;
+		int bufferSize = 1024 * 1024;
 		long remainingBytes = 0;
 		byte[] buffer = null;
 		String tmp1, tmp2;
-
-		
 
 		while (true) {
 			// initialization block
 			try {
 				printWriter = new PrintWriter(socket.getOutputStream());
 				inputStream = new BufferedInputStream(socket.getInputStream());
-//				System.out.println("Init success");
-			} catch (IOException e1) {
-				FileServer.disconnectClient(detail.getName());
-//				System.out.println("Client is disconnected");
-//				FileServer.statistics.removeActiveUers();
-				break;
-			}
-			
-			// receiving file details as json String
-			try {
 				buffer = new byte[bufferSize];
-				// System.out.println("Receiving file details as string");
-				try {
-					inputStream.read(buffer);
-					fileDetail = new String(buffer);
-					fileDetail=fileDetail.substring(0, fileDetail.lastIndexOf('|'));
-//					System.out.println(fileDetail);
-					
+				// System.out.println("Init success");
+
+				
+				
+				
+			// System.out.println("Receiving file details as string");
+				inputStream.read(buffer);
+				data = new String(buffer);
+				data = data.substring(0, data.lastIndexOf('|'));
+				// System.out.println(fileDetail);
+
+				// handling data
+				if (data.startsWith("INFO")) {
+					fileDetail = data.substring(4);
+
+					// sending file
+
+					// getting data from file details
 					// string tokenizer
+					StringTokenizer stringTokenizer = new StringTokenizer(fileDetail, ",");
+					file = new HashMap<String, String>();
+					while (stringTokenizer.hasMoreTokens()) {
+						StringTokenizer stringTokenizer2 = new StringTokenizer(stringTokenizer.nextToken(), ":");
+						tmp1 = stringTokenizer2.nextToken();
+						tmp2 = stringTokenizer2.nextToken();
+						file.put(tmp1, tmp2);
+						// System.out.println(tmp1 + "----" + tmp2 +
+						// "----");
+					}
+
+					System.out.println("File Details received");
+					System.out.println("details received ");
+
+					// new path
+					owner = file.get("owner");
+					filename = gettimestamp() + "-" + file.get("name");
+					// System.out.println(owner + "----" + filename);
+					String path = "C:\\Users\\Administrator\\Desktop\\";
+					path += owner + "\\" + filename;
+
+					// creating folder
+					new File("C:\\Users\\Administrator\\Desktop\\" + owner).mkdirs();
+					System.out.println("New Path-" + path);
+
+					// creating output path
+					outputStream = new BufferedOutputStream(new FileOutputStream(new File(path)));
+
+					// Sending ACK
+					printWriter.println("ACK OBJ");
+					printWriter.flush();
+					System.out.println("ACK OBJ SENT");
+
+					// getting file size
+
+					// Receiving file
+					System.out.println(file.get("filesize") + "--hi");
+					filesize = Long.parseLong(file.get("filesize"));
+					remainingBytes = filesize;
+					System.out.println("Receiving file");
 					try {
-						StringTokenizer stringTokenizer = new StringTokenizer(fileDetail, ",");
-						file = new HashMap<String, String>();
-						while (stringTokenizer.hasMoreTokens()) {
-							StringTokenizer stringTokenizer2 = new StringTokenizer(stringTokenizer.nextToken(), ":");
-							tmp1 = stringTokenizer2.nextToken();
-							tmp2 = stringTokenizer2.nextToken();
-							file.put(tmp1, tmp2);
-//							System.out.println(tmp1 + "----" + tmp2 + "----");
+						while (true) {
+							// System.out.println("Reading from stream");
+							bytesRead = inputStream.read(buffer, 0, (int) Math.min(bufferSize, remainingBytes));
+
+							// System.out.println(bytesRead + " bytes
+							// received");
+							if (bytesRead < 0 || remainingBytes <= 0)
+								break;
+							else
+								remainingBytes = remainingBytes - bytesRead;
+
+							// System.out.println(remainingBytes + " bytes
+							// remaining");
+							outputStream.write(buffer, 0, bytesRead);
+							// System.out.println(bytesRead + " bytes written to
+							// file");
 						}
 					} catch (Exception e) {
+						// e.printStackTrace();
 						FileServer.disconnectClient(detail.getName());
-//						System.out.println("Invalid details");
-//						System.out.println("Client is disconnected");
-//						FileServer.statistics.removeActiveUers();
-//						e.printStackTrace();
+						FileServer.statistics.addDataUploaded(filesize - remainingBytes);
+						// System.out.println("Client is disconnected");
+						// FileServer.statistics.removeActiveUers();
 						break;
 					}
 
-				} catch (Exception e) {
-					FileServer.disconnectClient(detail.getName());
-//					System.out.println(detail.getName() + " is disconnected");
-					detail.setOnline(false);
-//					FileServer.statistics.removeActiveUers();
-					break;
-					// e.printStackTrace();
-				}
-				System.out.println("File Details received");
-				System.out.println("details received ");
+					outputStream.flush();
+					System.out.println("File received success fully");
+					FileServer.statistics.addDataUploaded(filesize);
 
-				// new path
-				owner = file.get("owner");
-				filename = gettimestamp() + "-" + file.get("name");
-//				System.out.println(owner + "----" + filename);
-				String path = "C:\\Users\\Administrator\\Desktop\\";
-				path += owner + "\\" + filename;
+					// verification using checksum
+					// sending ACK TO Client
+					System.out.println("Comparing checksum");
+					if (Checksum.getChecksum(path).equals(file.get("checksum"))) {
+						System.out.println("File both are same");
+						FileServer.statistics.addFilesUploaded();
+						// success message to client
+						printWriter.println("SUCCESS " + path);
 
-				// creating folder
-				new File("C:\\Users\\Administrator\\Desktop\\" + owner).mkdirs();
-				System.out.println("New Path-" + path);
+						// adding to details
+						FileDetail fileDetail2 = new FileDetail(filename, path);
+						detail.getFiles().add(fileDetail2);
 
-				// creating output path
-				outputStream = new BufferedOutputStream(new FileOutputStream(new File(path)));
-
-				// Sending ACK
-				printWriter.println("ACK OBJ");
-				printWriter.flush();
-				System.out.println("ACK OBJ SENT");
-
-				// getting file size
-
-				// Receiving file
-				System.out.println(file.get("filesize") + "--hi");
-				filesize = Long.parseLong(file.get("filesize"));
-				remainingBytes = filesize;
-				System.out.println("Receiving file");
-				try {
-					while (true) {
-						// System.out.println("Reading from stream");
-						bytesRead = inputStream.read(buffer, 0, (int) Math.min(bufferSize, remainingBytes));
-						
-						// System.out.println(bytesRead + " bytes received");
-						if (bytesRead < 0 || remainingBytes <= 0)
-							break;
-						else
-							remainingBytes = remainingBytes - bytesRead;
-
-						// System.out.println(remainingBytes + " bytes remaining");
-						outputStream.write(buffer, 0, bytesRead);
-						// System.out.println(bytesRead + " bytes written to file");
+					} else {
+						// Sending failure response
+						System.out.println("File are not same");
+						System.out.println("File transfer Incomplete");
+						printWriter.println("TRANSFER FAILED");
 					}
-				} catch (Exception e) {
-//					e.printStackTrace();
+
+					printWriter.flush();
+					System.out.println("File transfer message sent to client");
+					FileServer.printStatistics();
+					outputStream.close();
+
+				} else if (data.equals("INFO")) {
+					//retrive file details
+					//parse it as string (JSON format)
+					//send to client
+				} else if (data.equals("END")) {
+					System.out.println("Client " + detail.getName() + " requested for end connection");
 					FileServer.disconnectClient(detail.getName());
-					FileServer.statistics.addDataUploaded(filesize-remainingBytes);
-//					System.out.println("Client is disconnected");
-//					FileServer.statistics.removeActiveUers();
+					socket.close();
 					break;
+				} else{
+					System.out.println("Invalid request:"+data);
 				}
 
-				outputStream.flush();
-				System.out.println("File received success fully");
-				FileServer.statistics.addDataUploaded(filesize);
-				
-				// verification using checksum
-				// sending ACK TO Client
-				System.out.println("Comparing checksum");
-				if (Checksum.getChecksum(path).equals(file.get("checksum"))) {
-					System.out.println("File both are same");
-					FileServer.statistics.addFilesUploaded();
-					// success message to client
-					printWriter.println("SUCCESS " + path);
-					
-					//adding to details
-					FileDetail fileDetail2=new FileDetail(filename, path);
-					detail.getFiles().add(fileDetail2);
-					
-
-				} else {
-					// Sending failure response
-					System.out.println("File are not same");
-					System.out.println("File transfer Incomplete");
-					printWriter.println("TRANSFER FAILED");
-				}
-
-				printWriter.flush();
-				System.out.println("File transfer message sent to client");
-				FileServer.printStatistics();
-				outputStream.close();
-				// }
-			} catch (IOException e) {
-//				System.out.println("Client is disconnected");
-//				FileServer.statistics.removeActiveUers();
-//				e.printStackTrace();
+			} catch (Exception e) {
 				FileServer.disconnectClient(detail.getName());
+				// System.out.println(detail.getName() + " is
+				// disconnected");
+				detail.setOnline(false);
+				// FileServer.statistics.removeActiveUers();
+				break;
+				// e.printStackTrace();
 			}
 
 			// clearing buffer
